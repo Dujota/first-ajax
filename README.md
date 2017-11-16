3d4283a49a8c71d7c31639ae96de741a2506ab37
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-16 00:14:34 -0500

    created AJAX request with a empty data object and type text, with .done(responseData) that appends a p tag to the section with id step3456. used jquery to select the id for append

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+<h3>Step 0 - Setup and Knowledge Check</h3>
+1. Request Method:GET
+2. 85ms
+3. Status Code:200 OK
+4. normal HTML
--- ajax.js
+++ ajax.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  /* Your code goes here */
+  document.getElementById('step12').addEventListener('click', (e) => {
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/",
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    });
+  });
+
+  // can use jquery here too $('#step3456')
+  document.getElementById('step3456').addEventListener('click', (e) => {
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/ping",
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    }).done((responseData) => {
+      console.log(responseData);
+      let p = document.createElement('p'); // create a paragraph tag
+      p.innerText = responseData; // give the p tag responseData
+      $("#step3456").append(p); // use jquery to select the id and append the p with the responseData 
+    });
+  });
+
+
 
 });
