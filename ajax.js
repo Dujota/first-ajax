b2443f87a04c9ad37855c2a25a58225ec80eef65
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-16 23:11:09 -0500

    finished step 9, used jquery to select and append the response data that contained the html in one shot instead of creating the element and appending each item

--- ajax.js
+++ ajax.js
@@ -20,10 +20,62 @@ document.addEventListener("DOMContentLoaded", function() {
       console.log(responseData);
       let p = document.createElement('p'); // create a paragraph tag
       p.innerText = responseData; // give the p tag responseData
-      $("#step3456").append(p); // use jquery to select the id and append the p with the responseData 
+      $("#step3456").append(p); // use jquery to select the id and append the p with the responseData
+    }).fail( () => { // this will execute if there is a server error
+      console.log("this request fucked up and broke, womp womp womp!");
+      let fail = document.createElement('p');
+      fail.innerText = "Oh Uh, SumTing dun Goof'd. Giv'r anod'r shawt! :P"; //error message that will display if there is an issue with the request
+      $("#step3456").append(fail);
+    }).always( () => {
+      console.log("Hey the request finished!");
     });
   });
 
+  document.getElementById('step7').addEventListener('click', (e) => {
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/count",
+      method: 'GET',
+      data: {},
+      dataType: 'text'
+    }).done((responseData) => {
+      console.log(responseData);
+      let p = document.createElement('p');
+      p.innerText = responseData;
+      $("#step7").append(p);
+    }).fail( () => {
+      console.log("this request fucked up and broke, womp womp womp!");
+      let fail = document.createElement('p');
+      fail.innerText = "Oh Uh, SumTing dun Goof'd. Giv'r anod'r shawt! :P";
+      $("#step7").append(fail);
+    }).always( () => {
+      console.log("Hey the request finished!");
+    });
+  });
 
+  document.getElementById('step8').addEventListener('click', (e) => {
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/time",
+      method: 'GET',
+      data: {timezone: 'Pacific/Honolulu'},
+      dataType: 'text'
+    }).done((responseData) => {
+      let time = document.createElement('p');
+      time.innerText = responseData
+      $("#step8").append(time);
+    });
+  });
+
+  document.getElementById('step9').addEventListener('click', (e) => {
+    // console.log('clicked');
+    $.ajax({
+      url: "http://first-ajax-api.herokuapp.com/a_car",
+      method: 'GET',
+      data: {},
+      dataType: 'html'
+    }).done( (responseData) => {
+      // console.log(responseData);
+      $('#step9').append(responseData);
+    });
+  });
 
 });
